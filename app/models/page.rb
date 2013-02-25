d4771dcb4d58373c8269e9b7f6904f43877e8260
jwrathall <wrathallj@gmail.com>
2013-02-25 16:18:51 +0100

    added validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,11 @@ class Page < ActiveRecord::Base
   belongs_to :subject
   has_many :sections
   has_and_belongs_to_many :editors, :class_name => "AdminUser"
+  
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 50
+  validates_presence_of :permalink
+  validates_length_of :permalink, :withing => 3..100
+  validates_uniqueness_of :permalink
+  #for unique values by subject, :scope => :subject_id
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -6,6 +6,10 @@ class Subject < ActiveRecord::Base
   
   #validation
   validates_presence_of :name
+  validates_length_of :name, :maximum => 50
+  #we could also use :minimum => 1, this will give the error message "is too short"
+  #validates_presence_of gives "can't be blank"
+  #length_of allows strings with spaces
   
   scope :visible, where(:is_visible => true)
   scope :invisible, where(:is_visible => false)
